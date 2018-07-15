33cddecc8db9c2af04b7d19ddd72a340e8420aaa
Steve Daulton <stevedaulton@gmail.com>
2018-07-15 12:54:06 +0100

    Basic app with file browser. Also defines sizes of standard Tkinter fonts

--- helloworld.py
+++ helloworld.py
@@ -1,32 +1,78 @@
 #!/usr/bin/env python3
 
+"""Experiments with tkinter in Python3"""
+
 import tkinter as tk
-import tkinter.font
+import tkinter.font as fnt
+from tkinter import filedialog as fd
 
-class Application(tk.Frame):
-   def __init__(self, master=None):
-      super().__init__(master)
-      self.pack()
-      self.create_widgets()
 
-   def create_widgets(self):
-      self.hi_there = tk.Button(self)
-      self.hi_there["text"] = "Hello World\n(click me)"
-      self.hi_there["command"] = self.say_hi
-      self.hi_there.pack(side="top")
+class Application(tk.Frame):    # pylint: disable=too-many-ancestors
+    """Main application."""
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+        self.filename = ""
 
-      self.quit = tk.Button(self, text="QUIT", fg="red",
-                           command=root.destroy)
-      self.quit.pack(side="bottom")
+    def create_widgets(self):
+        """Create widgets"""
+        self.select_file = tk.Button(self)
+        self.select_file["text"] = "Select file"
+        self.select_file["command"] = self.get_file
+        self.select_file.pack(side="top")
 
-   def say_hi(self):
-      print("hi there, everyone!")
+        self.quit = tk.Button(self, text="Quit", fg="red",
+                              overrelief="groove",
+                              command=root.destroy)
+        self.quit["activebackground"] = "red"
+        self.quit["activeforeground"] = "#fff"
+        self.quit.pack(side="bottom")
+
+    def get_file(self):
+        """File selector dialog/"""
+        self.filename = fd.askopenfilename(initialdir="~/",
+                                           title="Select file",
+                                           filetypes=(("jpeg files", "*.jpg"),
+                                                      ("all files", "*.*")))
+        print(self.filename)
+
 
 root = tk.Tk()
-# Increase default font size to 10px
-default_font = tk.font.nametofont("TkDefaultFont")
-default_font.configure(family='sans-serif', size=10)
-#Scale widgets to 144 dpi (default scaling of x1 = 72 dpi)
-root.tk.call('tk', 'scaling', 2.0)
+
+# Hack to enable "show hidden files" button.
+try:
+    # call a dummy dialog with an impossible option to initialize the file
+    # dialog without really getting a dialog window; this will throw a
+    # TclError, so we need a try...except :
+    try:
+        root.tk.call('tk_getOpenFile', '-invalid')
+    except tk.TclError:
+        pass
+    # now set the magic variables accordingly
+    root.tk.call('set', '::tk::dialog::file::showHiddenBtn', '1')
+    root.tk.call('set', '::tk::dialog::file::showHiddenVar', '0')
+#The above calls are undocumented. They work on Linux now, but ...
+except tk.TclError as tke:
+    print(tke)
+    raise
+
+# Set standard font sizes
+fnt.nametofont("TkDefaultFont").configure(family='sans', size=10)
+fnt.nametofont("TkTextFont").configure(family='sans', size=10)
+fnt.nametofont("TkFixedFont").configure(family='monospace', size=10)
+fnt.nametofont("TkMenuFont").configure(family='sans', size=10)
+fnt.nametofont("TkHeadingFont").configure(family='sans', size=10)
+fnt.nametofont("TkCaptionFont").configure(family='sans', size=10)
+fnt.nametofont("TkSmallCaptionFont").configure(family='sans', size=8)
+fnt.nametofont("TkTooltipFont").configure(family='sans', size=9)
+fnt.nametofont("TkIconFont").configure(family='sans', size=10)
+
+#Scaling the widgets does not appear to be necessary, so long as we set the default
+#font size in points.
+#Scale widgets. Tkinter seems to assume a screen height of about 600 px
+#screen_height = root.winfo_screenheight()
+#root.tkinter.call('tk', 'scaling', screen_height / 600)
+
 app = Application(master=root)
 app.mainloop()
